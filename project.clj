0b4fb1678863bc37246ac2e9506cdf975a5ae8f9
dragicalj <gagaljubisavljevic111@gmail.com>
2023-04-24 11:40:38 +0200

    Loaded data on ballet dancers from a CSV file

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject ballet-injury-prediction "0.1.0-SNAPSHOT"
-  :description "A ballet injury prediction app using linear regression"
+  :description "A ballet injury prediction app using K-Nearest Neighbor(KNN) algorithm"
   :url "https://github.com/dragicalj/ballet-injury-prediction"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/data.csv "1.0.1"]
+                 [org.clojure/clojure "1.10.1"]]
+  :repositories [["clojars" "https://clojars.org/repo/"]
+                 ["central" "https://repo1.maven.org/maven2/"]]
   :repl-options {:init-ns ballet-injury-prediction.core})
--- src/ballet_injury_prediction/core.clj
+++ src/ballet_injury_prediction/core.clj
@@ -1,6 +1,23 @@
 (ns ballet-injury-prediction.core)
+(require '[clojure.data.csv :as csv])
+(require '[clojure.java.io :as io])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn load-csv [file]
+  (with-open [reader (io/reader file)]
+    (->> (csv/read-csv reader)
+         (mapv vec))))
+
+(def my-csv-file "data/ballet-dancers.csv")
+
+(load-csv my-csv-file)
+
+(def ballet-data (load-csv my-csv-file))
+
+(doseq [row (take 10 ballet-data)]
+  (println row))
+
+
+
+
+
+
